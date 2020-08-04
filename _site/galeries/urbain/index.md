3a75d8d1a7bd79e6199ff901d19535a6afce509e
SimonYung <simonquere24@gmail.com>
2020-08-04 23:18:44 +0200

    removed _site from gitignore

---
+++ _site/galeries/urbain/index.md
@@ -0,0 +1,20 @@
+---
+layout: "gallery.njk"
+title: "ORDINAIRE(S) URBAIN(S) RURBAIN(S)"
+description: ""
+cover : ""
+collection: "{{ page.fileSlug }}"
+products:
+#   les images produits son dans le dossier "products"
+#   - image: nom_de_l_image.jpg
+#   - link: https://www.pcagallery.com/example
+  - image: L1010158.jpg
+    link: https://www.pcagallery.com
+---
+# Description :
+
+Ceci est la dscription de cette gallerie, c'est un exemple.
+**ce texte est gras** *ce texte est italique* ~~ce texte est rayé~~.
+[Ceci est un hyperlien](https:www.google.com/).
+
+> Ceci est une citation
---
+++ _site/includes/assets/js/masonry_resize.js
@@ -0,0 +1,63 @@
+function resize(className,scale) {
+
+    items = document.getElementsByClassName(className);
+    for (i = 0; i < items.length ; i++) {
+        let ratio = (items[i].naturalWidth / items[i].naturalHeight) * scale
+        let flexBasis = ratio + 'px';
+        items[i].style.flex = flexBasis;
+        items[i].style.flexShrink = '0';
+    };
+  
+}
+
+function resizeLastLine() {
+
+    let lastItem = items[items.length - 1].getBoundingClientRect().top;
+
+    for (i = items.length - 1; i > -1; i--) {
+        let currItem = items[i].getBoundingClientRect().top;
+        if ( lastItem == currItem) {
+            items[i].style.flexGrow = '0';
+        } else{
+            items[i].style.flexGrow = '1';
+        }
+    };
+}
+
+
+var items;
+window.onload = function(event){
+    const scalingRatio = Number( document.getElementById('gallery').dataset.scalingRatio );
+    resize('preview', scalingRatio);
+    resizeLastLine();
+};
+
+
+var myEfficientFn = debounce(function() {
+
+    //actual work goes here vvv
+    
+        resizeLastLine();
+    
+    //actual work goes here ^^^
+    
+    }, 250);
+    
+    //rebound function vvv
+    
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+    
+window.addEventListener('resize', myEfficientFn);
