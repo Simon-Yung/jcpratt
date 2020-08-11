961f670f94542b6be3aef6997fd19f96f6d218da
SimonYung <simonquere24@gmail.com>
2020-08-11 19:09:29 +0200

    missing assets

---
+++ docs/includes/assets/js/masonry_resize_lite.js
@@ -0,0 +1,23 @@
+function resize(className,scale) {
+
+    //i could have remade this toonly set flex basis and a width 
+    //and just call resize last line to resize flex growth
+
+    items = document.getElementsByClassName(className);
+    for (i = 0; i < items.length ; i++) {
+        let ratio = (items[i].naturalWidth * scale / items[i].naturalHeight);
+        let flexBasis = ratio + 'px';
+        items[i].style.width = flexBasis; //because google won't behave
+        items[i].style.flex = flexBasis;
+        items[i].style.flexGrow = ratio;
+        items[i].style.flexShrink = '0';
+    };
+  
+}
+
+var items;
+const scalingRatio = Number( document.getElementById('gallery').dataset.scalingRatio );
+window.onload = function(event){
+    resize('preview', scalingRatio);
+};
+
---
+++ docs/includes/assets/js/modal_gallery.js
@@ -0,0 +1,137 @@
+
+// Get the modal gallery by id
+const numberOfImage = document.getElementsByClassName('preview').length;
+var currentImage;
+var modalIsOpen = false;
+
+const modal = document.getElementById('modal_window');
+
+const imageContainer = document.getElementById('modal_image_container');
+const image = document.getElementById('modal_image');
+
+const purchaseLink = document.getElementById('purchase_link');
+const previous = document.getElementById('previous');
+const pagination = document.getElementById('pagination');
+const next = document.getElementById('next');
+const closeButton = document.getElementById('close');
+
+const menu = document.getElementById("contextual_menu");
+const menu_next = document.getElementById('menu_next');
+const menu_previous = document.getElementById('menu_previous');
+const menu_close = document.getElementById('menu_close');
+var menuIsVisible = false;
+
+//fake loader
+const loader = document.getElementById('spinner');
+function spinIt() {
+    loader.style.height = "100%";
+    loader.classList.add('spinning');
+    loader.onanimationend = () => {
+        loader.classList.remove('spinning');
+        loader.style.height = "0px";
+    };
+}
+
+// Modal
+
+function openModalGallery(imageIDNumber) {
+    //looking back , i could have just used an array instead of this id system, oh well...
+    modalIsOpen = true;
+    //check if image id is within bound, wrap around if not
+    imageIDNumber = parseInt(imageIDNumber);
+    if ( imageIDNumber == 0 ) { imageIDNumber = numberOfImage; }
+    if ( imageIDNumber > numberOfImage ) { imageIDNumber = 1; }
+    let img;
+    img = document.getElementById(imageIDNumber);
+    //animate the preview when clicked
+    img.classList.add("zoom");
+    img.onanimationend = () => {
+        img.classList.remove("zoom");
+    };
+    //update the buy button
+    if ( img.dataset.externalLink == undefined){
+        purchaseLink.style.display = 'none';
+    } else{
+        purchaseLink.style.display = 'flex';
+        purchaseLink.href = img.dataset.externalLink;
+    }
+    //change image, open the modal
+    modal.style.display = "flex";
+    pagination.innerHTML = img.dataset.pagination;
+    image.src = img.src.replace("/thumbnails", "/larges");
+    currentImage = imageIDNumber;
+
+    //spinner action
+    spinIt();
+}
+
+//prev and next
+function nextModal(n) {
+  currentImage += n;
+  openModalGallery(currentImage);
+}
+
+//custom right click menu
+
+function hideMenu(){
+    if (menuIsVisible){
+        menu.style.display = 'none';
+        menuIsVisible = false;
+    }
+};
+
+function showMenu( top, left ){
+    menu.style.top = top + 'px';
+    menu.style.left = left + 'px';
+    menu.style.display = 'flex';
+    menuIsVisible = true;
+};
+
+
+// EVENTLISTENER
+
+// FOR THE MENU
+
+addEventListener( 'click' , function(event) {
+    hideMenu();
+});
+addEventListener('contextmenu',function(event){
+    if (modalIsOpen){
+        let mouseX = event.clientX;
+        let mouseY = event.clientY;
+        event.preventDefault();
+        hideMenu();
+        showMenu(mouseY , mouseX);
+    }
+});
+
+// MENU CONTROLS
+menu_previous.addEventListener( 'click' , function(event) {
+    nextModal(-1);
+});
+menu_next.addEventListener( 'click' , function(event) {
+    nextModal(+1);
+});
+menu_close.addEventListener( 'click' , function(event) {
+    modal.style.display = "none";
+    modalIsOpen = false;
+});
+
+// MODAL CONTROLS
+
+closeButton.addEventListener("click", function(){
+    modal.style.display = "none";
+    modalIsOpen = false;
+}); 
+previous.addEventListener("click", function(){
+      nextModal(-1);
+}); 
+next.addEventListener("click", function(){
+      nextModal(+1);
+}); 
+imageContainer.addEventListener("click", function(){
+    if (!menuIsVisible){
+        modal.style.display = "none";
+    }
+    modalIsOpen = false;
+}); 
